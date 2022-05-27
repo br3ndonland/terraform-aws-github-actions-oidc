9a341210f229cfeb2fccbaaf9033179852319f8d
Brendon Smith <bws@bws.bio>
2022-05-27 13:54:49 -0400

    Support multiple repos

--- main.tf
+++ main.tf
@@ -1,23 +1,27 @@
 locals {
-  oidc_client_ids         = ["sts.amazonaws.com"]
-  oidc_issuer_domain      = "token.actions.githubusercontent.com"
-  oidc_subject_conditions = ["repo:${var.github_org}/${var.github_repo}:${var.github_custom_claim}"]
+  github_repos       = { for repo in var.github_repos : replace(repo, "/", "-") => repo }
+  oidc_client_ids    = ["sts.amazonaws.com"]
+  oidc_issuer_domain = "token.actions.githubusercontent.com"
 }
 
-# Create OIDC provider
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_openid_connect_provider
+# Fetch TLS certificate thumbprint from OIDC provider
+
 data "tls_certificate" "github" {
   url = "https://${local.oidc_issuer_domain}/.well-known/openid-configuration"
 }
+
+# Create a single GitHub Actions OIDC provider
+
 resource "aws_iam_openid_connect_provider" "github" {
   client_id_list  = local.oidc_client_ids
   thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
   url             = "https://${local.oidc_issuer_domain}"
 }
 
-# Define resource-based role trust policy for IAM role
-# https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_identity-vs-resource.html
+# Define resource-based role trust policy for each IAM role
+
 data "aws_iam_policy_document" "role_trust_policy" {
+  for_each = local.github_repos
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity", "sts:TagSession"]
     principals {
@@ -32,15 +36,19 @@ data "aws_iam_policy_document" "role_trust_policy" {
     condition {
       test     = "StringLike"
       variable = "${local.oidc_issuer_domain}:sub"
-      values   = local.oidc_subject_conditions
+      values   = ["repo:${each.value}:${var.github_custom_claim}"]
     }
   }
 }
 
-# Create role for OIDC provider and attach resource-based role trust policy
-# https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-idp.html
+# Create IAM roles for each repo and attach a role trust policy to each role
+
 resource "aws_iam_role" "github_actions_oidc" {
-  name               = var.aws_iam_role_name
-  description        = "IAM role with role trust policy defining how the role can be assumed"
-  assume_role_policy = data.aws_iam_policy_document.role_trust_policy.json
+  for_each           = local.github_repos
+  assume_role_policy = data.aws_iam_policy_document.role_trust_policy[each.key].json
+  description        = "IAM assumed role for GitHub Actions in the ${each.value} repo"
+  name = join(
+    var.aws_iam_role_separator,
+    flatten([var.aws_iam_role_prefix, split("/", each.value)])
+  )
 }
