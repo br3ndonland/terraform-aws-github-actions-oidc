8224a9ee2b6b2dea54805387107d27781fc4a275
Brendon Smith <bws@bws.bio>
2023-06-24 14:03:13 -0400

    Add support for custom IAM role names
    This Terraform module provisions IAM roles for GitHub repos and enables
    the repos to connect GitHub Actions and AWS using OpenID Connect (OIDC).
    Roles are named with acustomizable name prefix followed by the
    GitHub owner and repo names.
    
    This naming strategy becomes problematic for long GitHub names because
    AWS restricts IAM role names to 64 characters. Longer IAM role names
    raise validation errors through the Terraform AWS provider.
    
    To avoid errors seen with long GitHub repo names, this commit will
    introduce support for custom IAM role names. An input variable
    `aws_iam_role_names` will be added. The variable accepts a mapping of
    GitHub repo names and custom role names. The input variable will include
    a validation rule that ensures each role name is 64 characters or less.
    
    This input variable benefits from an extended description that would
    result in a long single-line string, so a heredoc will be used for the
    description. The use of heredocs in input variable descriptions appears
    to be supported but undocumented (terraform-docs/terraform-docs#471).
    
    https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html
    https://github.com/hashicorp/terraform-provider-aws/blob/c67e7e0289a8f0443903c4d287acefc74db7f3a7/internal/service/iam/role.go#L141
    https://developer.hashicorp.com/terraform/language/expressions/strings
    https://developer.hashicorp.com/terraform/language/values/variables
    https://developer.hashicorp.com/terraform/language/expressions/custom-conditions

--- main.tf
+++ main.tf
@@ -1,5 +1,28 @@
 locals {
-  github_repos       = { for repo in var.github_repos : replace(repo, "/", "-") => repo }
+  /* Retain case in IAM role names if no separator
+  This module supports an input variable that defines the character used to separate
+  words in the IAM role name. IAM roles are commonly named in either `PascalCase` or
+  `lowercase-with-separators`. This local preserves case if there is no separator
+  and if the role name prefix is written in mixed-case (`GitHubActionsOIDCOwnerRepo`),
+  or lowercases the name if there is a separator (`github-actions-oidc-owner-repo`). */
+  aws_iam_role_name_defaults = {
+    for repo in var.github_repos :
+    lower(replace(repo, "/", "-")) => join(
+      var.aws_iam_role_separator,
+      flatten(
+        [
+          var.aws_iam_role_prefix,
+          split(
+            "/",
+            var.aws_iam_role_prefix != lower(var.aws_iam_role_prefix) && var.aws_iam_role_separator == ""
+            ? repo
+            : lower(repo)
+          )
+        ]
+      )
+    )
+  }
+  github_repos       = { for repo in var.github_repos : lower(replace(repo, "/", "-")) => repo }
   oidc_client_ids    = ["sts.amazonaws.com"]
   oidc_issuer_domain = "token.actions.githubusercontent.com"
 }
@@ -49,8 +72,9 @@ resource "aws_iam_role" "github_actions_oidc" {
   for_each           = local.github_repos
   assume_role_policy = data.aws_iam_policy_document.role_trust_policy[each.key].json
   description        = "IAM assumed role for GitHub Actions in the ${each.value} repo"
-  name = join(
-    var.aws_iam_role_separator,
-    flatten([var.aws_iam_role_prefix, split("/", each.value)])
+  name = (
+    length(lookup(var.aws_iam_role_names, each.value, "")) != 0
+    ? substr(var.aws_iam_role_names[each.value], 0, 64)
+    : substr(local.aws_iam_role_name_defaults[each.key], 0, 64)
   )
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+variable "aws_iam_role_names" {
+  description = <<-DESCRIPTION
+    Optional mapping of GitHub repos to names of IAM roles that will be assumed by GitHub for OIDC.
+    If set, the IAM role name for the repo will be exactly the variable value (64 characters max).
+
+    Example: `{ "really-long-github-org-name/really-long-github-repo-name" = "custom-role-name" }`
+
+    If unset, the default IAM role name for each repo will be
+    `<aws_iam_role_prefix><aws_iam_role_separator><repo_owner><aws_iam_role_separator><repo_name>`,
+    truncated to 64 characters.
+  DESCRIPTION
+  type        = map(string)
+  default     = {}
+  validation {
+    condition     = alltrue([for key, value in var.aws_iam_role_names : length(value) <= 64])
+    error_message = "The IAM role name must be less than or equal to 64 characters."
+  }
+}
+
 variable "aws_iam_role_prefix" {
   description = "Prefix for name of IAM role that will be assumed by GitHub for OIDC"
   type        = string
