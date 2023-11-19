bad0e10e076d49b988eaf23dca54d08775b803c4
Brendon Smith <bws@bws.bio>
2023-11-18 22:10:42 -0500

    Remove Terraform Cloud state backend example

--- examples/s3/terraform.tf
+++ examples/s3/terraform.tf
@@ -1,11 +1,4 @@
 terraform {
-  cloud {
-    hostname     = "app.terraform.io"
-    organization = "<YOUR_TERRAFORM_CLOUD_ORG>"
-    workspaces {
-      name = "aws-github-actions-oidc"
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
